37d426269218b37aeb154eac6e93ea072936b865
km140484d <mkonjikovac12@gmail.com>
2019-11-06 23:09:53 +0100

    Pokusaj GNB

--- logisticka.py
+++ logisticka.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from dateutil.parser._parser import _ymd
+import seaborn as sn
 
 
 def h(theta, x):
@@ -49,7 +49,13 @@ def delta(x, y, theta):
 
 def gauss(x, my, sigma):
     sigma2 = math.pow(sigma, 2)
-    return 1 / math.sqrt(2*math.pi*sigma2) * math.exp(-math.pow((x - my), 2) / 2 * sigma2)
+    return 1 / math.sqrt(2 * math.pi * sigma2) * math.exp(-math.pow((x - my), 2) / 2 * sigma2)
+
+
+def gnb(x, my1, sigma1, my0, sigma0):
+    invS1, invS0 = np.linalg.inv(sigma1), np.linalg.inv(sigma0)
+    return math.exp(0.5*x.T.dot(invS1).dot(x) - my1.T.dot(invS1).dot(x) + 0.5*my1.T.dot(invS1).dot(my1)
+                    - 0.5*x.T.dot(invS0).dot(x) + my0.T.dot(invS0).dot(x) - 0.5*my0.T.dot(invS0).dot(my0))
 
 
 df = pd.read_csv('multiclass_data.csv', header=None)
@@ -74,7 +80,6 @@ y0[y0 >= 1], y0[y0 == 0], y0[y0 > 1] = 2, 1, 0
 y1[y1 != 1] = 0
 y2[y2 <= 1], y2[y2 == 2] = 0, 1
 
-
 # theta0, theta1, theta2 = gradient_descent(xs, y0), gradient_descent(xs, y1), gradient_descent(xs, y2)
 # conf = np.zeros((k, k))
 # y_guess = np.zeros((m, 1), int)
@@ -92,11 +97,11 @@ y2[y2 <= 1], y2[y2 == 2] = 0, 1
 # print('conf [log]: ', conf)
 #
 # # softmax
-# alpha, step, row_num, cnt = 0.02, 0, 10, 1000
+# alpha, step, row_num, cnt = 0.02, 0, 10, 1000  # 10,20,30
 # theta = np.zeros((k, n))
 # shuffle = np.arange(m)
 # for i in range(cnt):
-#     theta = theta + alpha*delta(xs[step:min(m, step+row_num)], y[step:min(m, step+row_num)], theta)
+#     theta = theta + alpha * delta(xs[step:min(m, step + row_num)], y[step:min(m, step + row_num)], theta)
 #     step = (step + row_num) % m
 #     if step < row_num:
 #         step = 0
@@ -109,16 +114,19 @@ y2[y2 <= 1], y2[y2 == 2] = 0, 1
 #     for r in range(k):
 #         s = s + math.exp(theta[r].dot(xs[i].T))
 #     for r in range(k):
-#         phi[r] = math.exp(theta[r].dot(xs[i].T))/s
+#         phi[r] = math.exp(theta[r].dot(xs[i].T)) / s
 #     phi_max_index = np.argmax(phi)
 #     conf[y[i], phi_max_index] = conf[y[i], phi_max_index] + 1
 # print('conf [softmax]: ', conf)
 
-# Gausovska diskriminantna analiza
+# GDA - Gausovska diskriminantna analiza
 xs = xs[:, 1:]
 xs = np.c_[xs, y]
-n = n-1     # nema potrebe vise za kolonom sa 1
+n = n - 1  # nema potrebe vise za kolonom sa 1
 xs0, xs1, xs2 = xs[np.where(xs[:, n] == 0)], xs[np.where(xs[:, n] == 1)], xs[np.where(xs[:, n] == 2)]
+xs0, xs1, xs2 = xs0[:, :-1], xs1[:, :-1], xs2[:, :-1]
+print(xs0.shape[0], xs0.shape[1])
+print(xs0)
 x_sep = [xs0, xs1, xs2]
 my, sigma = np.zeros((k, n)), np.zeros((k, n))
 # racunanje my-matematicko ocekivanje, sigma-standardna devijansa
@@ -126,16 +134,47 @@ for i in range(k):
     for j in range(n):
         my[i, j] = np.mean(x_sep[i][:, j])
         sigma[i, j] = np.std(x_sep[i][:, j])
-conf = np.zeros((k, k))
-for i in range(m):
-    gm, p = np.zeros((k, n)), np.zeros(k)   # gauss matrix
-    total = 0
-    for l in range(k):
-        for j in range(n):
-            gm[l, j] = gauss(xs[i, j], my[l, j], sigma[l, j])
-        p[l] = np.prod(gm[l])
-        total = total + p[l]
-    p = p / total
-    conf[y[i], np.argmax(p)] = conf[y[i], np.argmax(p)] + 1
-print('conf: ', conf)
 
+conf = np.zeros((k, k))
+# for i in range(m):
+#     gm, p = np.zeros((k, n)), np.zeros(k)  # gauss matrix
+#     total = 0
+#     for l in range(k):
+#         for j in range(n):
+#             gm[l, j] = gauss(xs[i, j], my[l, j], sigma[l, j])
+#         p[l] = np.prod(gm[l])
+#         total = total + p[l]
+#     p = p / total
+#     conf[y[i], np.argmax(p)] = conf[y[i], np.argmax(p)] + 1
+# print('conf [gauss]: ', conf)
+# df_cm = pd.DataFrame(conf, range(k), range(k))
+# hm = sn.heatmap(df_cm, annot=True, annot_kws={"size": 12})
+# bottom, top = hm.get_ylim()
+# hm.set_ylim(bottom + 0.5, top - 0.5)
+#
+# plt.show()
+
+# GNB - Naivni Bayes
+
+# print(my.shape[0], my.shape[1])
+# print(my[0].T.dot(np.ones((1, xs0.shape[0]))))
+MY0 = np.ones((5, xs0.shape[0]))
+MY1 = np.ones((5, xs1.shape[0]))
+MY2 = np.ones((5, xs2.shape[0]))
+for j in range(n):
+    MY0[j] = my[0, j]
+    MY1[j] = my[1, j]
+    MY2[j] = my[2, j]
+SIGMA0 = 1 / (xs0.shape[0] - 1) * (xs0.T - MY0).dot((xs0.T - MY0).T)
+SIGMA1 = 1 / (xs1.shape[0] - 1) * (xs1.T - MY1).dot((xs1.T - MY1).T)
+SIGMA2 = 1 / (xs2.shape[0] - 1) * (xs2.T - MY2).dot((xs2.T - MY2).T)
+
+conf = np.zeros((k, k))
+xs = xs[:, :-1]
+for i in range(m):
+    p = np.zeros(k)
+    p[0] = 1 / (1 + gnb(xs[i].T, my[1].T, SIGMA1, my[0].T, SIGMA0) + gnb(xs[i].T, my[2].T, SIGMA2, my[0].T, SIGMA0))
+    p[1] = 1 / (1 + gnb(xs[i].T, my[0].T, SIGMA0, my[1].T, SIGMA1) + gnb(xs[i].T, my[2].T, SIGMA2, my[1].T, SIGMA1))
+    p[2] = 1 / (1 + gnb(xs[i].T, my[0].T, SIGMA0, my[2].T, SIGMA2) + gnb(xs[i].T, my[1].T, SIGMA1, my[2].T, SIGMA2))
+    conf[y[i], np.argmax(p)] = conf[y[i], np.argmax(p)] + 1
+print('conf [gnb]: ', conf)
