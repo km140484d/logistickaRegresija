087d201bbaabc6db4ff7b1ff76933d589913b19c
km140484d <mkonjikovac12@gmail.com>
2019-11-06 19:23:56 +0100

    Gausovska diskriminantna analiza

--- logisticka.py
+++ logisticka.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from dateutil.parser._parser import _ymd
 
 
 def h(theta, x):
@@ -37,23 +38,28 @@ def gradient_descent(x, y):
 def delta(x, y, theta):
     m = x.shape[0]
     deltaJ = np.zeros((k, n))
-    for r in range(k-1):
+    for r in range(k - 1):
         for i in range(m):
             s = 0
             for j in range(k):
                 s = s + math.exp(theta[j].dot(x[i].T))
-            deltaJ[r] = deltaJ[r] + ((y[i] == r) - math.exp(theta[r].dot(x[i].T))/s) * x[i]    #
+            deltaJ[r] = deltaJ[r] + ((y[i] == r) - math.exp(theta[r].dot(x[i].T)) / s) * x[i]  #
     return deltaJ
+
+
+def gauss(x, my, sigma):
+    sigma2 = math.pow(sigma, 2)
+    return 1 / math.sqrt(2*math.pi*sigma2) * math.exp(-math.pow((x - my), 2) / 2 * sigma2)
 
 
 df = pd.read_csv('multiclass_data.csv', header=None)
 df.columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'y']
 df.insert(0, 'one', 1)
-df = df.sample(frac=1)
+# df = df.sample(frac=1)
 
 y = df['y'].to_numpy()
 x = df.iloc[:, 0:6].to_numpy()
-m, n, k = x.shape[0], x.shape[1], len(np.unique(y))     # n ukljucuje kolonu sa 1
+m, n, k = x.shape[0], x.shape[1], len(np.unique(y))  # n ukljucuje kolonu sa 1
 xs = np.copy(x)
 # standardizacija
 for i in range(1, 6):
@@ -62,49 +68,74 @@ for i in range(1, 6):
     for j in range(m):
         xs[j, i] = (xs[j, i] - xa) / std
 
+# # logisticka regresija
 y0, y1, y2 = np.copy(y), np.copy(y), np.copy(y)
 y0[y0 >= 1], y0[y0 == 0], y0[y0 > 1] = 2, 1, 0
 y1[y1 != 1] = 0
 y2[y2 <= 1], y2[y2 == 2] = 0, 1
-theta0, theta1, theta2 = gradient_descent(xs, y0), gradient_descent(xs, y1), gradient_descent(xs, y2)
 
-# konfuziona matrica
+
+# theta0, theta1, theta2 = gradient_descent(xs, y0), gradient_descent(xs, y1), gradient_descent(xs, y2)
+# conf = np.zeros((k, k))
+# y_guess = np.zeros((m, 1), int)
+# for i in range(m):
+#     h0 = h(theta0, xs[i].T)
+#     h1 = h(theta1, xs[i].T)
+#     h2 = h(theta2, xs[i].T)
+#     if h0 > h1 and h0 > h2:
+#         y_guess[i] = 0
+#     elif h1 > h0 and h1 > h2:
+#         y_guess[i] = 1
+#     else:
+#         y_guess[i] = 2
+#     conf[y[i], y_guess[i]] = conf[y[i], y_guess[i]] + 1
+# print('conf [log]: ', conf)
+#
+# # softmax
+# alpha, step, row_num, cnt = 0.02, 0, 10, 1000
+# theta = np.zeros((k, n))
+# shuffle = np.arange(m)
+# for i in range(cnt):
+#     theta = theta + alpha*delta(xs[step:min(m, step+row_num)], y[step:min(m, step+row_num)], theta)
+#     step = (step + row_num) % m
+#     if step < row_num:
+#         step = 0
+#         np.random.shuffle(shuffle)
+#         xs, y = xs[shuffle], y[shuffle]
+# conf = np.zeros((k, k))
+# for i in range(m):
+#     phi = np.zeros((k, 1))
+#     s = 0
+#     for r in range(k):
+#         s = s + math.exp(theta[r].dot(xs[i].T))
+#     for r in range(k):
+#         phi[r] = math.exp(theta[r].dot(xs[i].T))/s
+#     phi_max_index = np.argmax(phi)
+#     conf[y[i], phi_max_index] = conf[y[i], phi_max_index] + 1
+# print('conf [softmax]: ', conf)
+
+# Gausovska diskriminantna analiza
+xs = xs[:, 1:]
+xs = np.c_[xs, y]
+n = n-1     # nema potrebe vise za kolonom sa 1
+xs0, xs1, xs2 = xs[np.where(xs[:, n] == 0)], xs[np.where(xs[:, n] == 1)], xs[np.where(xs[:, n] == 2)]
+x_sep = [xs0, xs1, xs2]
+my, sigma = np.zeros((k, n)), np.zeros((k, n))
+# racunanje my-matematicko ocekivanje, sigma-standardna devijansa
+for i in range(k):
+    for j in range(n):
+        my[i, j] = np.mean(x_sep[i][:, j])
+        sigma[i, j] = np.std(x_sep[i][:, j])
 conf = np.zeros((k, k))
-y_guess = np.zeros((m, 1), int)
 for i in range(m):
-    h0 = h(theta0, xs[i].T)
-    h1 = h(theta1, xs[i].T)
-    h2 = h(theta2, xs[i].T)
-    if h0 > h1 and h0 > h2:
-        y_guess[i] = 0
-    elif h1 > h0 and h1 > h2:
-        y_guess[i] = 1
-    else:
-        y_guess[i] = 2
-    conf[y[i], y_guess[i]] = conf[y[i], y_guess[i]] + 1
+    gm, p = np.zeros((k, n)), np.zeros(k)   # gauss matrix
+    total = 0
+    for l in range(k):
+        for j in range(n):
+            gm[l, j] = gauss(xs[i, j], my[l, j], sigma[l, j])
+        p[l] = np.prod(gm[l])
+        total = total + p[l]
+    p = p / total
+    conf[y[i], np.argmax(p)] = conf[y[i], np.argmax(p)] + 1
+print('conf: ', conf)
 
-print('conf [log]: ', conf)
-
-# softmax
-alpha, step, row_num, cnt = 0.02, 0, 10, 1000
-theta = np.zeros((k, n))
-shuffle = np.arange(m)
-for i in range(cnt):
-    theta = theta + alpha*delta(xs[step:min(m, step+row_num)], y[step:min(m, step+row_num)], theta)
-    step = (step + row_num) % m
-    if step < row_num:
-        step = 0
-        np.random.shuffle(shuffle)
-        xs = xs[shuffle]
-        y = y[shuffle]
-conf = np.zeros((k, k))
-for i in range(m):
-    phi = np.zeros((k, 1))
-    s = 0
-    for r in range(k):
-        s = s + math.exp(theta[r].dot(xs[i].T))
-    for r in range(k):
-        phi[r] = math.exp(theta[r].dot(xs[i].T))/s
-    phi_max_index = np.argmax(phi)
-    conf[y[i], phi_max_index] = conf[y[i], phi_max_index] + 1
-print('conf [softmax]: ', conf)
