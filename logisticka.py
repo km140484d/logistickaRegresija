37e58a40526a3679aeeaacbe49d5ad0e06d8881c
km140484d <mkonjikovac12@gmail.com>
2019-11-08 14:57:02 +0100

    Konacno

--- logisticka.py
+++ logisticka.py
@@ -18,8 +18,7 @@ def standardization(x):
 
 
 def h(theta, x):
-    gamma = -theta.T.dot(x)
-    return 1 / (1 + math.exp(gamma))
+    return 1 / (1 + math.exp(-theta.T.dot(x)))
 
 
 # sarzni gradijentni spust, maksimizacija verodostojnosti
@@ -47,7 +46,6 @@ def gradient_loss(x, y):
 
 
 def gradient_descent_j(x, y, alpha=0.02, flag=0):
-    print('gradient_descent')
     theta = np.zeros((n, 1))
     bound = 2e-2
     J = []
@@ -118,74 +116,74 @@ x = df.iloc[:, 0:6].to_numpy()
 m, n, k = x.shape[0], x.shape[1], len(np.unique(y))  # n ukljucuje kolonu sa 1
 xs = standardization(x)
 
-# # logisticka regresija
-# y0, y1, y2 = np.copy(y), np.copy(y), np.copy(y)
-# y0[y0 >= 1], y0[y0 == 0], y0[y0 > 1] = 2, 1, 0
-# y1[y1 != 1] = 0
-# y2[y2 <= 1], y2[y2 == 2] = 0, 1
-# theta0, theta1, theta2 = gradient_descent(xs, y0), gradient_descent(xs, y1), gradient_descent(xs, y2)
-# conf_train, conf_test = np.zeros((k, k)), np.zeros((k, k))
-# y_guess = np.zeros((m, 1), int)
-# for i in range(m):
-#     h0, h1, h2 = h(theta0, xs[i].T), h(theta1, xs[i].T), h(theta2, xs[i].T)
-#     if h0 > h1 and h0 > h2:
-#         y_guess[i] = 0
-#     elif h1 > h0 and h1 > h2:
-#         y_guess[i] = 1
-#     else:
-#         y_guess[i] = 2
-#     if i < boundary_index:
-#         conf_train[y[i], y_guess[i]] = conf_train[y[i], y_guess[i]] + 1
-#     else:
-#         conf_test[y[i], y_guess[i]] = conf_test[y[i], y_guess[i]] + 1
-# plot_conf(conf_train, 'LOGISTIČKA:', 1)
-# plot_conf(conf_test, 'LOGISTIČKA:', 0)
-# gradient_loss(xs, y1)   # funkcija gubitka u zavisnosti od stope ucenja
+# logisticka regresija
+y0, y1, y2 = np.copy(y), np.copy(y), np.copy(y)
+y0[y0 >= 1], y0[y0 == 0], y0[y0 > 1] = 2, 1, 0
+y1[y1 != 1] = 0
+y2[y2 <= 1], y2[y2 == 2] = 0, 1
+theta0, theta1, theta2 = gradient_descent(xs, y0), gradient_descent(xs, y1), gradient_descent(xs, y2)
+conf_train, conf_test = np.zeros((k, k)), np.zeros((k, k))
+y_guess = np.zeros((m, 1), int)
+for i in range(m):
+    h0, h1, h2 = h(theta0, xs[i].T), h(theta1, xs[i].T), h(theta2, xs[i].T)
+    if h0 > h1 and h0 > h2:
+        y_guess[i] = 0
+    elif h1 > h0 and h1 > h2:
+        y_guess[i] = 1
+    else:
+        y_guess[i] = 2
+    if i < boundary_index:
+        conf_train[y[i], y_guess[i]] = conf_train[y[i], y_guess[i]] + 1
+    else:
+        conf_test[y[i], y_guess[i]] = conf_test[y[i], y_guess[i]] + 1
+plot_conf(conf_train, 'LOGISTIČKA:', 1)
+plot_conf(conf_test, 'LOGISTIČKA:', 0)
+gradient_loss(xs, y1)   # funkcija gubitka u zavisnosti od stope ucenja
 
-# # softmax
-# shuffle = np.arange(m)
-# row_num = [5, 10, 20]
-# row_size = row_num[1]
-# for row in row_num:
-#     alpha, step, cnt = 0.02, 0, 1000
-#     theta_row, J = np.zeros((k, n)), []
-#     for i in range(cnt):
-#         theta_row = theta_row + alpha * delta(xs[step:min(m, step + row)], y[step:min(m, step + row)], theta_row)
-#         dJ = 0
-#         for i in range(m):
-#             y_guess = 0
-#             for j in range(k):
-#                 y_guess = y_guess + math.exp(theta_row[j].dot(xs[i].T))
-#             dJ = dJ + (theta_row[y[i]].dot(xs[i].T) - math.log(y_guess))
-#         J.append(-dJ)
-#         step = (step + row) % m
-#         if step < row:
-#             step = 0
-#             np.random.shuffle(shuffle)
-#             xs, y = xs[shuffle], y[shuffle]
-#     if row == row_size:
-#         theta = theta_row
-#     plt.plot(range(len(J)), J)
-# plt.legend(['šarža = 5', 'šarža = 10', 'šarža = 20'], loc='upper right')
-# plt.xlabel('iter')
-# plt.ylabel('J')
-# plt.show()
-# conf_train, conf_test = np.zeros((k, k)), np.zeros((k, k))
-# for i in range(m):
-#     phi, s = np.zeros((k, 1)), 0
-#     for r in range(k):
-#         phi[r] = math.exp(theta[r].dot(xs[i].T))
-#         s = s + math.exp(theta[r].dot(xs[i].T))
-#     phi = phi / s
-#     phi_max_index = np.argmax(phi)
-#     if i < boundary_index:
-#         conf_train[y[i], phi_max_index] = conf_train[y[i], phi_max_index] + 1
-#     else:
-#         conf_test[y[i], phi_max_index] = conf_test[y[i], phi_max_index] + 1
-# plot_conf(conf_train, 'SOFTMAX:', 1)
-# plot_conf(conf_test, 'SOFTMAX:', 0)
+# softmax
+shuffle = np.arange(m)
+row_num = [5, 10, 20]
+row_size = row_num[1]
+for row in row_num:
+    alpha, step, cnt = 0.02, 0, 1000
+    theta_row, J = np.zeros((k, n)), []
+    for i in range(cnt):
+        theta_row = theta_row + alpha * delta(xs[step:min(m, step + row)], y[step:min(m, step + row)], theta_row)
+        dJ = 0
+        for i in range(m):
+            y_guess = 0
+            for j in range(k):
+                y_guess = y_guess + math.exp(theta_row[j].dot(xs[i].T))
+            dJ = dJ + (theta_row[y[i]].dot(xs[i].T) - math.log(y_guess))
+        J.append(-dJ)
+        step = (step + row) % m
+        if step < row:
+            step = 0
+            np.random.shuffle(shuffle)
+            xs, y = xs[shuffle], y[shuffle]
+    if row == row_size:
+        theta = theta_row
+    plt.plot(range(len(J)), J)
+plt.legend(['šarža = 5', 'šarža = 10', 'šarža = 20'], loc='upper right')
+plt.xlabel('iter')
+plt.ylabel('J')
+plt.show()
+conf_train, conf_test = np.zeros((k, k)), np.zeros((k, k))
+for i in range(m):
+    phi, s = np.zeros((k, 1)), 0
+    for r in range(k):
+        phi[r] = math.exp(theta[r].dot(xs[i].T))
+        s = s + math.exp(theta[r].dot(xs[i].T))
+    phi = phi / s
+    phi_max_index = np.argmax(phi)
+    if i < boundary_index:
+        conf_train[y[i], phi_max_index] = conf_train[y[i], phi_max_index] + 1
+    else:
+        conf_test[y[i], phi_max_index] = conf_test[y[i], phi_max_index] + 1
+plot_conf(conf_train, 'SOFTMAX:', 1)
+plot_conf(conf_test, 'SOFTMAX:', 0)
 
-# # GDA - Gausovska diskriminantna analiza
+# GDA - Gausovska diskriminantna analiza
 xs = xs[:, 1:]
 xs = np.c_[xs, y]
 n = n - 1  # nema potrebe vise za kolonom sa 1
@@ -193,27 +191,27 @@ xs0, xs1, xs2 = xs[np.where(xs[:, n] == 0)], xs[np.where(xs[:, n] == 1)], xs[np.
 xs0, xs1, xs2 = xs0[:, :-1], xs1[:, :-1], xs2[:, :-1]
 x_sep = [xs0, xs1, xs2]
 my, sigma = np.zeros((k, n)), np.zeros((k, n))
-# # racunanje my-matematicko ocekivanje, sigma-standardna devijansa
-# for i in range(k):
-#     for j in range(n):
-#         my[i, j] = np.mean(x_sep[i][:, j])
-#         sigma[i, j] = np.sd(x_sep[i][:, j])
-# conf_train, conf_test = np.zeros((k, k)), np.zeros((k, k))
-# for i in range(m):
-#     gm, p = np.zeros((k, n)), np.zeros(k)  # gauss matrix
-#     total = 0
-#     for l in range(k):
-#         for j in range(n):
-#             gm[l, j] = gauss(xs[i, j], my[l, j], sigma[l, j])
-#         p[l] = np.prod(gm[l])
-#         total = total + p[l]
-#     p = p / total
-#     if i < boundary_index:
-#         conf_train[y[i], np.argmax(p)] = conf_train[y[i], np.argmax(p)] + 1
-#     else:
-#         conf_test[y[i], np.argmax(p)] = conf_test[y[i], np.argmax(p)] + 1
-# plot_conf(conf_train, 'GDA:', 1)
-# plot_conf(conf_test, 'GDA:', 0)
+# racunanje my-matematicko ocekivanje, sigma-standardna devijansa
+for i in range(k):
+    for j in range(n):
+        my[i, j] = np.mean(x_sep[i][:, j])
+        sigma[i, j] = np.std(x_sep[i][:, j])
+conf_train, conf_test = np.zeros((k, k)), np.zeros((k, k))
+for i in range(m):
+    gm, p = np.zeros((k, n)), np.zeros(k)  # gauss matrix
+    total = 0
+    for l in range(k):
+        for j in range(n):
+            gm[l, j] = gauss(xs[i, j], my[l, j], sigma[l, j])
+        p[l] = np.prod(gm[l])
+        total = total + p[l]
+    p = p / total
+    if i < boundary_index:
+        conf_train[y[i], np.argmax(p)] = conf_train[y[i], np.argmax(p)] + 1
+    else:
+        conf_test[y[i], np.argmax(p)] = conf_test[y[i], np.argmax(p)] + 1
+plot_conf(conf_train, 'GDA:', 1)
+plot_conf(conf_test, 'GDA:', 0)
 
 # GNB - Naivni Bayes
 MY0 = np.ones((5, xs0.shape[0]))
@@ -223,6 +221,7 @@ for j in range(n):
     MY0[j] = my[0, j]
     MY1[j] = my[1, j]
     MY2[j] = my[2, j]
+    print(xs0, my)
 SIGMA0 = 1 / (xs0.shape[0] - 1) * (xs0.T - MY0).dot((xs0.T - MY0).T)
 SIGMA1 = 1 / (xs1.shape[0] - 1) * (xs1.T - MY1).dot((xs1.T - MY1).T)
 SIGMA2 = 1 / (xs2.shape[0] - 1) * (xs2.T - MY2).dot((xs2.T - MY2).T)
